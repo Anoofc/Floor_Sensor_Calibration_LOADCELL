71dc952fa0bab10deceaea3ded56e0d0928b655c
Anoof Chappangathil <anoof7262@gmail.com>
2024-05-11 17:05:58 +0400

    Initial commit

---
+++ Floor_sensor_calibration.ino
@@ -0,0 +1,55 @@
+
+#include "HX711.h"
+
+//21,22 -1st from left
+//18,19 -2nd from left
+//23,4 -3rd from left
+
+//32,33 -1st from right 
+//26,27 -2nd from right
+//13,25 -3rd from right
+#define LOADCELL_DOUT_PIN D2//13
+#define LOADCELL_SCK_PIN  D1//25
+
+
+HX711 scale;
+
+float calibration_factor = 1000;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("HX711 calibration sketch");
+  Serial.println("Remove all weight from scale");
+  Serial.println("After readings begin, place known weight on scale");
+  Serial.println("Press + or a to increase calibration factor");
+  Serial.println("Press - or z to decrease calibration factor");
+  
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.set_scale();
+  scale.tare(); //Reset the scale to 0
+  
+  long zero_factor = scale.read_average(); //Get a baseline reading
+  Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
+  Serial.println(zero_factor);
+}
+
+void loop() {
+ 
+  scale.set_scale(calibration_factor); //Adjust to this calibration factor
+  Serial.print("Reading: ");
+  Serial.print(scale.get_units(), 1);
+  Serial.print(" KG"); //Change this to kg and re-adjust the calibration factor if you follow SI units like a sane person
+  Serial.print(" calibration_factor: ");
+  Serial.print(calibration_factor);
+  Serial.println();
+
+  if(Serial.available())
+  {
+    char temp = Serial.read();
+    if(temp == '+' || temp == 'a')
+      calibration_factor += 10;
+    else if(temp == '-' || temp == 'z')
+      calibration_factor -= 10;
+  }
+}
